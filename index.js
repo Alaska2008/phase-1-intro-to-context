f701fc0968690ebaeea2aeb6029c0892297b9f07
Amen Atikpo <alaskale2024@gmail.com>
2022-06-26 08:25:18 +0000

    Context lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Your code here
+records =[
+    {}
+]
+function createEmployeeRecord(record){
+    return {
+        firstName: record[0],
+        familyName: record[1],
+        title: record[2],
+        payPerHour: record[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }  
+}
+function createEmployeeRecords(employeeData){
+    return employeeData.map(function(rows){
+        return createEmployeeRecord(rows)
+    })
+}
+function createTimeInEvent(dateStamp){
+    
+    
+}
+
+function createTimeOutEvent(){
+
+}
+
+function hoursWorkedOnDate(){
+
+}
+
+function hoursWorkedOnDate(){
+
+}
+
+function wagesEarnedOnDate(){
+
+}
+
+function allWagesFor(){
+
+}
+
+function calculatePayroll() {
+
+}
